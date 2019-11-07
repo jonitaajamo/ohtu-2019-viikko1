4b437b2d91c757cb4ed4b28731cbe8dcc23191e2
Joni Taajamo <joni.taajamo@outlook.com>
2019-11-07 21:41:51 +0200

    added codeclimate and checkstyle

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -5,7 +5,6 @@ import ohtu.ohtuvarasto.Varasto;
 public class Main {
 
     public static void main(String[] args) {
-
         Varasto mehua = new Varasto(100.0);
         Varasto olutta = new Varasto(100.0, 20.2);
 
@@ -56,5 +55,6 @@ public class Main {
         saatiin = mehua.otaVarastosta(-32.9);
         System.out.println("saatiin " + saatiin);
         System.out.println("Mehuvarasto: " + mehua);
+
     }
 }
